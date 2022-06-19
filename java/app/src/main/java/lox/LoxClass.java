31a142d58ac5652d0f14e0a69235c613101f526d
Colton Donnelly <colton@donn.io>
2022-06-19 03:03:57 -0400

    finish 12.7.0

--- java/app/src/main/java/lox/LoxClass.java
+++ java/app/src/main/java/lox/LoxClass.java
@@ -22,12 +22,19 @@ public class LoxClass implements LoxCallable {
 
 	@Override
 	public int arity() {
-		return 0;
+		var initializer = findMethod("init");
+		if (initializer == null)
+			return 0;
+		return initializer.arity();
 	}
 
 	@Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
 		var instance = new LoxInstance(this);
+		var initializer = findMethod("init");
+		if (initializer != null) {
+			initializer.bind(instance).call(interpreter, arguments);
+		}
 		return instance;
 	}
 
