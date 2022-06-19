4de22b3c87d5b9a3f49f01893a9124ed9ac83a80
Colton Donnelly <colton@donn.io>
2022-06-19 02:02:49 -0400

    finish 12.3

--- java/app/src/main/java/lox/Parser.java
+++ java/app/src/main/java/lox/Parser.java
@@ -28,6 +28,8 @@ public class Parser {
 
 	private Stmt declaration() {
 		try {
+			if (match(CLASS))
+				return classDeclaration();
 			if (match(FUN))
 				return function("function");
 			if (match(VAR))
@@ -38,6 +40,20 @@ public class Parser {
 			synchronize();
 			return null;
 		}
+	}
+
+	private Stmt.Class classDeclaration() {
+		var name = consume(IDENTIFIER, "Expect class name.");
+		consume(LEFT_BRACE, "Expect '{' before class body.");
+
+		var methods = new ArrayList<Stmt.Function>();
+		while (!check(RIGHT_BRACE) && !isAtEnd()) {
+			methods.add(function("method"));
+		}
+
+		consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+		return new Stmt.Class(name, methods);
 	}
 
 	private Stmt.Function function(String kind) {
