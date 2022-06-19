e169bfb0def683e4c9037a3befe95abcc2080755
Colton Donnelly <colton@donn.io>
2022-06-19 02:37:59 -0400

    finish 12.5

--- java/app/src/main/java/lox/Resolver.java
+++ java/app/src/main/java/lox/Resolver.java
@@ -81,6 +81,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 	public Void visitClassStmt(Stmt.Class stmt) {
 		declare(stmt.name);
 		define(stmt.name);
+
+		for (var method : stmt.methods) {
+			var declaration = FunctionType.METHOD;
+			resolveFunction(method, declaration);
+		}
+
 		return null;
 	}
 
@@ -226,4 +232,5 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 enum FunctionType {
 	NONE,
 	FUNCTION,
+	METHOD,
 }
