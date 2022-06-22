ccabe772aaa4e0df1bd0a64ce449cfa9659fafa4
Colton Donnelly <colton@donn.io>
2022-06-22 13:55:07 -0400

    finish 14.3

--- c/chunk.h
+++ c/chunk.h
@@ -6,4 +6,13 @@ typedef enum {
 	OP_RETURN,
 } OpCode;
 
+typedef struct {
+	int count;
+	int capacity;
+	uint8_t* code;
+} Chunk;
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
 #endif // clox_chunk_h
