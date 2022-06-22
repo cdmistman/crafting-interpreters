9539c286397c03eacff2093a810724414b39a8c8
Colton Donnelly <colton@donn.io>
2022-06-22 14:33:57 -0400

    finish 14.4

--- c/chunk.h
+++ c/chunk.h
@@ -12,7 +12,7 @@ typedef struct {
 	uint8_t* code;
 } Chunk;
 void initChunk(Chunk* chunk);
-void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+void freeChunk(Chunk* chunk);
 
 #endif // clox_chunk_h
