c98e12d0e5c5494a690c331d71eefa5553f26cf9
Colton Donnelly <colton@donn.io>
2022-06-23 12:01:35 -0400

    finish 14.6, fix formatting, fix bug from previous commit

--- c/chunk.c
+++ c/chunk.c
@@ -22,12 +22,13 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 	if (chunk->capacity < chunk->count + 1) {
 		int oldCapacity = chunk->capacity;
 		chunk->capacity = GROW_CAPACITY(oldCapacity);
-		chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+		chunk->code =
+				GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
 		chunk->lines = GROW_ARRAY(int, chunk->code, oldCapacity, chunk->capacity);
 	}
 
 	chunk->code[chunk->count] = byte;
-	chunk->code[chunk->count] = line;
+	chunk->lines[chunk->count] = line;
 	chunk->count++;
 }
 
--- c/value.c
+++ c/value.c
@@ -13,7 +13,8 @@ void writeValueArray(ValueArray* array, Value value) {
 	if (array->capacity < array->count + 1) {
 		int oldCapacity = array->capacity;
 		array->capacity = GROW_CAPACITY(oldCapacity);
-		array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+		array->values =
+				GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
 	}
 
 	array->values[array->count] = value;
@@ -24,6 +25,4 @@ void freeValueArray(ValueArray* array) {
 	FREE_ARRAY(Value, array->values, array->capacity);
 }
 
-void printValue(Value value) {
-	printf("%g", value);
-}
+void printValue(Value value) { printf("%g", value); }
