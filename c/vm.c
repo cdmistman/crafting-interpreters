b873a5d5be87d34a06e0c05f8b855d5d20b1ecf7
Colton Donnelly <colton@donn.io>
2022-06-26 00:25:05 -0400

    finish 15.3.0

--- c/vm.c
+++ c/vm.c
@@ -46,6 +46,9 @@ static InterpretResult run() {
 				push(constant);
 				break;
 			}
+			case OP_NEGATE:
+				push(-pop());
+				break;
 			case OP_RETURN:
 				printValue(pop());
 				printf("\n");
