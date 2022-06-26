00adc4cd7921ed46c4d506971d425c757cfd3570
Colton Donnelly <colton@donn.io>
2022-06-25 23:42:36 -0400

    finish 15.1.1

--- c/vm.h
+++ c/vm.h
@@ -5,9 +5,18 @@
 
 typedef struct {
 	Chunk* chunk;
+	// better to store in a local variable to keep in a register
+	uint8_t* ip;
 } VM;
+
+typedef enum {
+	INTERPRET_OK,
+	INTERPRET_COMPILE_ERROR,
+	INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk* chunk);
 
 #endif // clox_vm_h
