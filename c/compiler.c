c26683bbdb1c91cbe55e554b751e348dffdd64b8
Colton Donnelly <colton@donn.io>
2022-06-28 15:59:13 -0400

    finish 17.2.0

--- c/compiler.c
+++ c/compiler.c
@@ -4,9 +4,25 @@
 #include "compiler.h"
 #include "scanner.h"
 
+struct {
+	Token current;
+	Token previous;
+} parser;
+
+static void advance() {
+	parser.previous = parser.current;
+
+	for (;;) {
+		parser.current = scanToken();
+		if (parser.current.type != TOKEN_ERROR)
+			break;
+
+		errorAtCurrent(parser.current.start);
+	}
+}
+
 bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expect end of expression.");
-
