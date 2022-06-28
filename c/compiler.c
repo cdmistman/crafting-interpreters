c90c0a0a8fef635a50fca77e758817f7138a5d16
Colton Donnelly <colton@donn.io>
2022-06-28 17:02:23 -0400

    finish 17.6

--- c/compiler.c
+++ c/compiler.c
@@ -205,7 +205,21 @@ ParseRule rules[] = {
 		[TOKEN_EOF] = {NULL, NULL, PREC_NONE},
 };
 
-static void parsePrecedence(Precedence precedence) {}
+static void parsePrecedence(Precedence precedence) {
+	advance();
+	ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+	if (prefixRule == NULL) {
+		error("Expect expression.");
+		return;
+	}
+
+	prefixRule();
+	while (precedence <= getRule(parser.current.type)->precedence) {
+		advance();
+		ParseFn infixRule = getRule(parser.previous.type)->infix;
+		infixRule();
+	}
+}
 
 static ParseRule* getRule(TokenType type) { return &rules[type]; }
 
