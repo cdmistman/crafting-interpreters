c7580ed7c6fcf184404d790421a342d4708bb70f
Colton Donnelly <colton@donn.io>
2022-07-03 11:28:18 -0700

    I FIGURED OUT THE BUG IT WAS A COPY-PASTE BUG what a classic tbh

--- c/memory.c
+++ c/memory.c
@@ -9,15 +9,7 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 		return NULL;
 	}
 
-	void* result;
-	if (pointer == NULL || oldSize == 0) {
-		if (pointer == NULL ^ oldSize == 0) {
-			printf("uh oh");
-		}
-		result = malloc(newSize);
-	} else {
-		result = realloc(pointer, newSize);
-	}
+	void* result = realloc(pointer, newSize);
 	if (result == NULL)
 		exit(1);
 	return result;
