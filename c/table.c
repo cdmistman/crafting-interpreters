68b088e603f5040e0b991070fe6c48b14fafb51c
Colton Donnelly <colton@donn.io>
2022-07-04 13:02:46 -1000

    finish 20.4.2

--- c/table.c
+++ c/table.c
@@ -6,6 +6,20 @@
 #include "table.h"
 #include "vm.h"
 
+#define TABLE_MAX_LOAD 0.75
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+	uint32_t index = key->hash & capacity;
+	for (;;) {
+		Entry* entry = &entries[index];
+		if (entry->key == key || entry->key == NULL) {
+			return entry;
+		}
+
+		index = (index + 1) % capacity;
+	}
+}
+
 void initTable(Table* table) {
 	table->count = 0;
 	table->capacity = 0;
@@ -15,4 +29,19 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
 	FREE_ARRAY(Entry, table->entries, table->capacity);
 	initTable(table);
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+	if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+		int capacity = GROW_CAPACITY(table->capacity);
+		adjustCapacity(table, capacity);
+	}
+
+	Entry* entry = findEntry(table->entries, table->capacity, key);
+	bool isNewKey = entry->key == NULL;
+	if (isNewKey)
+		table->count++;
+	entry->key = key;
+	entry->value = value;
+	return isNewKey;
 }
