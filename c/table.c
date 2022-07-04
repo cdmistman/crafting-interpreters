cdb09aa50640087925897564e11847d517d2f744
Colton Donnelly <colton@donn.io>
2022-07-04 13:10:33 -1000

    finish 20.4.3

--- c/table.c
+++ c/table.c
@@ -20,6 +20,27 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
 	}
 }
 
+static void adjustCapacity(Table* table, int capacity) {
+	Entry* entries = ALLOCATE(Entry, capacity);
+	for (int i = 0; i < capacity; i++) {
+		entries[i].key = NULL;
+		entries[i].value = NIL_VAL;
+	}
+	for (int i = 0; i < table->capacity; i++) {
+		Entry* entry = &table->entries[i];
+		if (entry->key == NULL)
+			continue;
+
+		Entry* dest = findEntry(entries, capacity, entry->key);
+		dest->key = entry->key;
+		dest->value = entry->value;
+	}
+
+	FREE_ARRAY(Entry, table->entries, table->capacity);
+	table->entries = entries;
+	table->capacity = capacity;
+}
+
 void initTable(Table* table) {
 	table->count = 0;
 	table->capacity = 0;
@@ -44,4 +65,13 @@ bool tableSet(Table* table, ObjString* key, Value value) {
 	entry->key = key;
 	entry->value = value;
 	return isNewKey;
+}
+
+void tableAddAll(Table* from, Table* to) {
+	for (int i = 0; i < from->capacity; i++) {
+		Entry* entry = &from->entries[i];
+		if (entry->key != NULL) {
+			tableSet(to, entry->key, entry->value);
+		}
+	}
 }
