2d2d4790a92612bbc8b54c8797519fe656a50b46
Colton Donnelly <colton@donn.io>
2022-07-05 10:55:05 -1000

    finish 22.2

--- c/compiler.c
+++ c/compiler.c
@@ -138,6 +138,10 @@ static void endCompiler() {
 #endif // DEBUG_PRINT_CODE
 }
 
+static void beginScope() { current->scopeDepth++; }
+
+static void endScope() { current->scopeDepth--; }
+
 static void expression();
 static void statement();
 static void declaration();
@@ -337,6 +341,14 @@ static ParseRule* getRule(TokenType type) { return &rules[type]; }
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
+static void block() {
+	while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+		declaration();
+	}
+
+	consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
 	uint8_t global = parseVariable("Expect variable name.");
 	if (match(TOKEN_EQUAL)) {
@@ -399,6 +411,10 @@ static void declaration() {
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		printStatement();
+	} else if (match(TOKEN_LEFT_BRACE)) {
+		beginScope();
+		block();
+		endScope();
 	} else {
 		expressionStatement();
 	}
