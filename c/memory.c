d269badc32a8c34866bdd7ce6f296b00a6ceb0a3
Colton Donnelly <colton@donn.io>
2022-08-02 15:49:30 -0400

    finish 26.3.0

--- c/memory.c
+++ c/memory.c
@@ -27,6 +27,22 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 	return result;
 }
 
+void markObject(Obj* object) {
+	if (object == NULL)
+		return;
+#ifdef DEBUG_LOG_GC
+	printf("%p mark ", (void*)object);
+	printValue(OBJ_VAL(object));
+	printf("\n");
+#endif // DEBUG_LOG_GC
+	object->isMarked = true;
+}
+
+void markValue(Value value) {
+	if (IS_OBJ(value))
+		markObject(AS_OBJ(value));
+}
+
 static void freeObject(Obj* object) {
 #ifdef DEBUG_LOG_GC
 	printf("%p free type %d\n", (void*)object, object->type);
@@ -60,10 +76,19 @@ static void freeObject(Obj* object) {
 	}
 }
 
+static void markRoots() {
+	for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+		markValue(*slot);
+	}
+	markTable(&vm.globals);
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
 	printf("-- gc begin\n");
 #endif // DEBUG_LOG_GC
+
+	markRoots();
 
 #ifdef DEBUG_LOG_GC
 	printf("-- gc end\n");
