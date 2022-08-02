c9c737467170e2cb1ea6a037ad6e69af7498a5d6
Colton Donnelly <colton@donn.io>
2022-08-02 17:38:36 -0400

    finish 27.2

--- c/vm.c
+++ c/vm.c
@@ -380,6 +380,9 @@ static InterpretResult run() {
 				push(result);
 				frame = &vm.frames[vm.frameCount - 1];
 				break;
+			case OP_CLASS:
+				push(OBJ_VAL(newClass(READ_STRING())));
+				break;
 		}
 	}
 
