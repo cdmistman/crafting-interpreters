9d8ac956eaea572718eca82a17a8364f99687764
Colton Donnelly <colton@donn.io>
2022-08-02 17:32:32 -0400

    finish 27.1

--- c/memory.c
+++ c/memory.c
@@ -112,6 +112,10 @@ static void freeObject(Obj* object) {
 #endif // DEBUG_LOG_GC
 
 	switch (object->type) {
+		case OBJ_CLASS: {
+			FREE(ObjClass, object);
+			break;
+		}
 		case OBJ_CLOSURE: {
 			ObjClosure* closure = (ObjClosure*)object;
 			FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
